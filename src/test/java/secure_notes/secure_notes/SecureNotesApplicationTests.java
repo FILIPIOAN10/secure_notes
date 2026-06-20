d6e549596205b6d2a4719983a28a18dd10e8abfa
username <filipioan_29@yahoo.ro>
2026-06-20 11:13:47 +0300

    Initial commit - Spring Boot structure

---
+++ src/test/java/secure_notes/secure_notes/SecureNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package secure_notes.secure_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
