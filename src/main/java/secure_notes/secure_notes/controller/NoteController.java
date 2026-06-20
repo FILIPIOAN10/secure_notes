f5147c58db7884f96ac2ec330041909485c5527f
username <filipioan_29@yahoo.ro>
2026-06-20 18:32:53 +0300

    Create NoteController and fix PostgreSQL database connection

---
+++ src/main/java/secure_notes/secure_notes/controller/NoteController.java
@@ -0,0 +1,47 @@
+package secure_notes.secure_notes.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import secure_notes.secure_notes.models.Note;
+import secure_notes.secure_notes.service.NoteService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+@RequiredArgsConstructor
+public class NoteController {
+
+    private final NoteService noteService;
+
+    @PostMapping
+    public Note createNote(@RequestBody String content,
+                           @AuthenticationPrincipal UserDetails userDetails){
+        String username  = userDetails.getUsername();
+        System.out.println("USER DETAILS" +username);
+        return noteService.createNoteForUser(username,content);
+    }
+
+    @GetMapping
+    public List<Note> getUserNotes(@AuthenticationPrincipal UserDetails userDetails){
+        String userName = userDetails.getUsername();
+        return noteService.getNotesForUser(userName);
+    }
+
+    @PutMapping("/{noteId}")
+    public Note updateNote(@PathVariable Long noteId,
+                           @RequestBody String content,
+                           @AuthenticationPrincipal UserDetails userDetails){
+        String username = userDetails.getUsername();
+        return noteService.updateNoteForUser(noteId,content,username);
+    }
+
+    @DeleteMapping("/{noteId}")
+    public void deleteNote(@PathVariable Long noteId,
+                           @AuthenticationPrincipal UserDetails userDetails){
+        String username = userDetails.getUsername();
+        noteService.deleteNoteForUser(noteId,username);
+    }
+}
